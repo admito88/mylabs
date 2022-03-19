3ac8c1443e64adc9e0b838891cf9f1228ee962c2
Dykyi Vadim <admito88@gmail.com>
2022-03-19 12:26:39 -0700

    new edit in terraform file

--- GlobalLogic_task/aws_servers.tf
+++ GlobalLogic_task/aws_servers.tf
@@ -17,6 +17,24 @@ systemctl enable jenkins
 systemctl start jenkins
 EOF
   key_name      = "Amazon_linux"
+  tags = {
+    Name = "jenkins_server"
+  }
+  vpc_security_group_ids = [aws_security_group.jenkins_server.id]
+}
+
+resource "aws_instance" "kvm_server" {
+  ami           = "ami-0dcc0ebde7b2e00db"
+  instance_type = "t2.micro"
+  user_data     = <<EOFstall jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+EOF
+  key_name      = "Amazon_linux"
+  tags = {
+    Name = "jenkins_server"
+  }
+  vpc_security_group_ids = [aws_security_group.jenkins_server.id]
 }
 
 resource "aws_instance" "kvm_server" {
@@ -26,6 +44,53 @@ resource "aws_instance" "kvm_server" {
 #!/bin/bash
 yum update -y
 yum upgrade
+#!/bin/bash
+yum update -y
+yum upgrade
 EOF
   key_name      = "Amazon_linux"
+  tags = {
+    Name = "kvm_server"
+  }
+  vpc_security_group_ids = [aws_security_group.kvm_server.id]
+}
+resource "aws_security_group" "jenkins_server" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "jenkins_server"
+  }
+}
+resource "aws_security_group" "kvm_server" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "kvm_server"
+  }
 }
