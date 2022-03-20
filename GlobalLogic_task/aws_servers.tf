465654fb81377f5382f2fcd5278ae1c73449ab10
Dykyi Vadim <admito88@gmail.com>
2022-03-20 16:03:14 +0200

    add HAproxy server create in terraform file

--- GlobalLogic_task/aws_servers.tf
+++ GlobalLogic_task/aws_servers.tf
@@ -26,45 +26,41 @@ EOF
 resource "aws_instance" "kvm_server" {
   ami           = "ami-0dcc0ebde7b2e00db"
   instance_type = "t2.micro"
-  user_data     = <<EOFstall jenkins -y
-systemctl enable jenkins
-systemctl start jenkins
+  user_data     = <<EOF
+  #!/bin/bash
+  yum update -yexport AWS_SECRET_ACCESS_KEY=JRG6iBzcYevirYCVrZT2EKKL0ubKw1uQxmIMK0SQ
+  yum upgrade
 EOF
   key_name      = "Amazon_linux"
   tags = {
-    Name = "jenkins_server"
+    Name = "kvm_server"
   }
   vpc_security_group_ids = [aws_security_group.jenkins_server.id]
 }
 
-resource "aws_instance" "kvm_server" {
+resource "aws_instance" "HAproxy" {
   ami           = "ami-0dcc0ebde7b2e00db"
   instance_type = "t2.micro"
   user_data     = <<EOF
 #!/bin/bash
 yum update -y
 yum upgrade
-#!/bin/bash
-yum update -y
-yum upgrade
 EOF
   key_name      = "Amazon_linux"
   tags = {
-    Name = "kvm_server"
+    Name = "HAproxy_server"
   }
-  vpc_security_group_ids = [aws_security_group.kvm_server.id]
+  vpc_security_group_ids = [aws_security_group.HAproxy_server.id]
 }
 resource "aws_security_group" "jenkins_server" {
-dynamic "ingress" {
-  for_each = ["22", "8080"]
-  content {
-  from_port = ingress.value
-  to_port = ingress.value
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-}
-
+  dynamic "ingress" {
+    for_each = ["22", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
   egress {
     from_port   = 0
@@ -77,11 +73,35 @@ dynamic "ingress" {
   }
 }
 resource "aws_security_group" "kvm_server" {
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+  dynamic "ingress" {
+    for_each = ["22", "80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "kvm_server"
+  }
+}
+
+resource "aws_security_group" "HAproxy_server" {
+  dynamic "ingress" {
+    for_each = ["22", "80", "443", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
   egress {
     from_port   = 0
