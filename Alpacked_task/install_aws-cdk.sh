60f4d97d2f1bc7468e732da4102717de14b8a5d2
Dykyi Vadim <admito88@gmail.com>
2022-03-22 09:42:03 +0200

    add start_work_cdk and edit install_aws-cdk.sh

--- Alpacked_task/install_aws-cdk.sh
+++ Alpacked_task/install_aws-cdk.sh
@@ -3,5 +3,8 @@ apt-get install python3-pip -y
 pip install aws-cdk.cdk
 pip install awscli
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g aws-cdk
+apt-get install -y nodejs
+npm install -g aws-cdk
+npm install aws-cdk-lib
+npm install -g typescript
+npm audit fix
