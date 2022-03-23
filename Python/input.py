989ee1c5a967852babe9ccac4662aac680070dc5
Vadim <admito88@gmail.com>
2022-03-23 02:42:22 -0700

    add 2 new files on python

---
+++ Python/input.py
@@ -0,0 +1,6 @@
+#input only string type data
+
+name = input ('Tale my your name: ')
+age = input ('Tale my you age: ')
+
+print('Your name is ' + name + ' and your age is ' + age)
---
+++ Python/print+env.py
@@ -0,0 +1,28 @@
+#int
+number = 5
+age = 33
+
+#float
+fnumber = 5.7
+
+#str
+name = "Abraham"
+
+#bool
+status = True
+
+# Ekranirovanie
+print ( 'Hi "is a bad" peope!' )
+print ( "Hi \"is a bad\" peope!" )
+
+#Perevod Stroki
+print ('Hello\nWorld')
+#or
+print ('Hello')
+print ('World')
+
+#Konkotinatsya, ispolzovanie peremenuh v zaprose only for string
+print ('Hello, my name is ' + name + '!')
+
+#Konkotinatsya, ispolzovanie peremenuh v zaprose s izmeneniem typa peremennoy
+print ('My age ' + str(age) + ' for now')
